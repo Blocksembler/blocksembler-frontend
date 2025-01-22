1d15093da79094d670251674ac5d98a5fd1635c6
Florian Wörister <ffwoerister@gmail.com>
2025-01-22 21:35:03 +0100

    data directive block added
    implement transformation of %data directive to machine code

--- src/architectures/instructions.js
+++ src/architectures/instructions.js
@@ -1,6 +1,15 @@
 export class BaseInstruction {
-    constructor(args, label = null) {
+    constructor(args, label = null, comment = null) {
         this.args = args;
         this.label = label;
+        this.comment = comment;
+    }
+}
+
+export class PseudoInstruction {
+    constructor(args, label = null, comment = null) {
+        this.args = args;
+        this.label = label;
+        this.comment = comment;
     }
 }
