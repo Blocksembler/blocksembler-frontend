7035077c86da29bbe569f7e5cc4d9fcf303eac9f
Florian Wörister <ffwoerister@gmail.com>
2025-02-10 22:54:59 +0100

    allow user to reset memory or registers only

--- src/components/BaseDropDown.vue
+++ src/components/BaseDropDown.vue
@@ -1,19 +1,21 @@
 <script setup>
-defineProps(["items"]);
+let props = defineProps(["items"]);
 </script>
 
 <template>
-  <div class="btn-group m-3">
-    <button
-        aria-expanded="false"
-        class="btn btn-secondary dropdown-toggle"
-        data-bs-toggle="dropdown"
-        type="button"
-    >
-      ANNA
+  <div class="btn-group">
+    <button class="btn btn-secondary" type="button" @click="$emit('click', '__default__', $event)">
+      <slot></slot>
+    </button>
+    <button aria-expanded="false" class="btn btn-secondary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown"
+            type="button">
+      <span class="visually-hidden">Toggle Dropdown</span>
     </button>
     <ul class="dropdown-menu">
-      <li v-for="item in items" class="dropdown-item">{{ item.name }}</li>
+      <li v-for="item in items">
+        <a class="dropdown-item" href="#" @click="$emit('click', item.key, $event)"> {{ item.label }}</a>
+      </li>
     </ul>
   </div>
+
 </template>
