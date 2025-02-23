344f62de9fe3b9642b0039651be9b8b11f1e6fcd
Florian Wörister <31357619+fwoerister@users.noreply.github.com>
2025-02-23 18:57:18 +0100

    disable all plugins except armlet

--- src/architectures/pluginRegistry.js
+++ src/architectures/pluginRegistry.js
@@ -1,8 +1,6 @@
 import armlet from "@/architectures/armlet/plugin.js"
-import anna from "@/architectures/anna/plugin.js"
-import scott from "@/architectures/scott/plugin.js"
 
-const plugins = [armlet, anna, scott];
+const plugins = [armlet];
 
 export const pluginRegistry = plugins.reduce((registry, plugin) => {
     registry[plugin.name] = plugin;
