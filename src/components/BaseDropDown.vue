0c2fdb051f9031a7159e3bc0e3b02a7dcbb2fd8f
Florian Wörister <31357619+fwoerister@users.noreply.github.com>
2025-02-26 18:33:32 +0100

    fix reset dropdown button

--- src/components/BaseDropDown.vue
+++ src/components/BaseDropDown.vue
@@ -1,10 +1,21 @@
 <script setup>
-let props = defineProps(["items"]);
+import {computed} from "vue";
+
+const props = defineProps(["items"]);
+
+let default_item = computed(() => {
+  return props.items.filter(item => item.key === "__default__")[0];
+})
+
+const button_items = computed(() => {
+  return props.items.filter(elem => elem.key !== "__default__");
+})
+
 </script>
 
 <template>
   <div class="btn-group">
-    <button class="btn btn-secondary" type="button" @click="$emit('click', $event, '__default__')">
+    <button class="btn btn-secondary" type="button" @click="default_item.clickEvent">
       <slot></slot>
     </button>
     <button aria-expanded="false" class="btn btn-secondary dropdown-toggle dropdown-toggle-split"
@@ -13,8 +24,8 @@ let props = defineProps(["items"]);
       <span class="visually-hidden">Toggle Dropdown</span>
     </button>
     <ul class="dropdown-menu">
-      <li v-for="item in items">
-        <a class="dropdown-item" href="#" @click="$emit('click',  $event, '__default__')"> {{ item.label }}</a>
+      <li v-for="item in button_items">
+        <a class="dropdown-item" href="#" @click="item.clickEvent"> {{ item.label }}</a>
       </li>
     </ul>
   </div>
