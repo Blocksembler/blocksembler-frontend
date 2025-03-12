a9f65c68f9c43d7c828a9a2d639db5cce9b35242
Florian Wörister <31357619+fwoerister@users.noreply.github.com>
2025-03-12 20:14:06 +0100

    data usage consent banner implemented

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -11,25 +11,28 @@ let props = defineProps({
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h4>{{ props.title }}</h4>
-          <button
-              aria-label="Close"
-              class="btn-close"
-              data-bs-dismiss="modal"
-              type="button"
-          ></button>
+          <slot name="header">
+            <h4>{{ props.title }}</h4>
+            <button
+                aria-label="Close"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                type="button"/>
+          </slot>
         </div>
         <div class="modal-body">
           <slot></slot>
         </div>
         <div class="modal-footer">
-          <button
-              class="btn btn-secondary"
-              data-bs-dismiss="modal"
-              type="button"
-          >
-            Close
-          </button>
+          <slot name="buttons">
+            <button
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+                type="button"
+            >
+              Close
+            </button>
+          </slot>
         </div>
       </div>
     </div>
