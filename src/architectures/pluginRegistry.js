ff4cf724876f5cb289291e5c80ed6976eb11cb57
Florian Wörister <31357619+fwoerister@users.noreply.github.com>
2025-03-13 11:15:29 +0100

    make the architecture plugin configurable via an environment variable

--- src/architectures/pluginRegistry.js
+++ src/architectures/pluginRegistry.js
@@ -1,6 +1,7 @@
 import armlet from "@/architectures/armlet/plugin.js"
+import anna from "@/architectures/anna/plugin.js"
 
-const plugins = [armlet];
+const plugins = [armlet, anna];
 
 export const pluginRegistry = plugins.reduce((registry, plugin) => {
     registry[plugin.name] = plugin;
