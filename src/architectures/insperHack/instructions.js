ca8f36e805ed72c01bf12e97c8792c1894558083
rumschoki <amy3st@gmail.com>
2025-04-16 00:37:59 +0200

    first two mov tests successful

--- src/architectures/insperHack/instructions.js
+++ src/architectures/insperHack/instructions.js
@@ -1,4 +1,4 @@
-import { MovInstruction } from "../armlet/instructions";
+import { MovInstruction } from "../insperHack/instructions";
 import { BaseInstruction } from "../instructions";
 import {Word} from "../system";
 
@@ -9,7 +9,7 @@ export class InsperHackInstructionFactory {
     }
 
     getInstructionClassByMnemonic(mnemonic) {
-        return instructionClasses.filter((c) => c.mnemonic === mnemonic)[0];
+        return mnemonicToClass[mnemonic];
     }
 
     createFromOpCode(memory, address) {
@@ -26,13 +26,20 @@ export class InsperHackInstructionFactory {
         // is inc or dec
 
         // c-bits instructions (ALU-code -> a and c bits)
+        let aluCode = code.slice(3,10);
 
-
+        switch (aluCode) {
+            case '0101010':
+              return new MovInstruction;
+            // …
+            default:
+              break;
+          }
     }
     
     getInstructionClassByOpCode(opCode) {
         // find class c with identical opCode
-        return instructionClasses.filter((c) => c.opCode === opCode)[0];
+        return mnemonicToClass.filter((c) => c.opCode === opCode)[0];
     }
 }
 
@@ -57,11 +64,29 @@ export class InsperHackInstruction extends BaseInstruction {
     createDestCode(args) {
         pass
     }
+    // append 000 for no jump operations
+    noJump(code) {
+        code += "000";
+        return code;
+    }
+    getRegValue(system, operand) {
+        return system.registers[operand];
+    }
 
-}
+    getMemoryAddress(system) {
+        // get value of Memory
+        let address = system.registers['%A'].toUnsignedIntValue();
+        // set value
+        return system.memory[address];
+    }
 
-export class LeawInstruction extends InsperHackInstruction {
+    getImmediateValue(operand) {
+        return Word.fromSignedIntValue(Number(operand));
+    }
 
+    isMemoryAccess(param) {
+        return param.startsWith('(');
+    }
 }
 
 export class MovInstruction extends InsperHackInstruction {
@@ -73,8 +98,39 @@ export class MovInstruction extends InsperHackInstruction {
         '0110000': ['%A'],
         '1110000': ['(%A)']
     };
-    static argsToCcode = {
+    argsToCcode = {
         '$0': '0101010', //a+c Code
+        '$1': '0111111',
+        '$-1': '0111010',
+        '%D': '0001100',
+        '%A': '0110000',
+        '(%A)':'1110000'
+    };
+    static cCodeToDests = {
+        '000': [''],
+        '100': ['%A'],
+        '010': ['%D'],
+        '001': ['(%A)'],
+        '110': ['%A', '%D'],
+        '101': ['%A', '(%A)'],
+        '011': ['%D', '(%A)'],
+        '111': ['%A', '%D', '(%A)'],
+    };
+    createDestCodeFrom(args) {
+        let destCode = [0, 0, 0];
+        args = args.slice(1);
+        args.forEach(arg => {
+            if (arg === '%A') {
+                destCode[0] = 1;
+            }
+            if (arg === '%D') {
+                destCode[1] = 1;
+            }
+            if (arg === '(%A)') {
+                destCode[2] = 1;
+            }
+        });
+        return destCode.join('');
     }
     static matchesCode(code) {
         let memoryBit = this.extractMemoryBit(code);
@@ -82,118 +138,48 @@ export class MovInstruction extends InsperHackInstruction {
 
         return (memoryBit + opCode) in this.cCodeToArgs;
     }
-
-    static fromMachineCode(code) {
+    static fromMachineCode(code) { // IN-PROGRESS
         let memoryBit = this.extractMemoryBit(code);
         let opCode = this.extractOpCode(code);
+        let cCode = memoryBit + opCode;
 
-        let args = this.cCodeToArgs[memoryBit + opCode].concat(this.extractDestArgs(code));
+        let params = cCodeToArgs[cCode];
+
+        let dests = this.cCodeToDests[this.extractDestCode(code)];
+        let args = /*this.cCodeToArgs[memoryBit + opCode]*/params.concat(dests);
 
         return new MovInstruction(args);
     }
 
     toMachineCode() {
-        return "111" + argsToCcode[this.args[0]] + this.createDestCode() + "000";
-    }
-}
-
-export class AddwInstruction extends InsperHackInstruction {
-    static get mnemonic() {
-        return 'addw';
-    }
-    static get opCode() {
-        return '000010';
-    }
-
-    // generates assembly-text array args from code
-    static fromMachineCode(code) {
-        let params = this.setParams(code);
-        let dest = this.setDestinations(code);
-        let args = params.concat(dest);
-
-        return new AddwInstruction(args);
-    }
-
-    executeOn(system) {
-        // operand 1 reg/mem
-        let op1Word;
-        if (this.isMemoryAccess(this.op1)) {
-            op1Word = this.getMemoryValue(system);
-        } else {
-            op1Word = this.getRegValue(system, this.op1);
-        };
-        // operand 2 (op2) reg/mem/im
-        let op2Word;
-        if (this.isMemoryAccess(this.op2)) {
-            op2Word = this.getMemoryValue(system);
-        } else if (this.op2.startsWith('%'))  { 
-            op2Word = this.getRegValue(system, this.op2);
-        } else { 
-            op2Word = this.getImmediateValue(this.op2);
-        }
-
-        this.dest.forEach((dest) => { 
-            // destinations one of mem/reg
-            let destWord;
-            if (this.isMemoryAccess(dest)) {
-                destWord = this.getMemoryValue(system);
-            } else {
-                destWord = this.getRegValue(system, dest);
-            }
-            // set result word
-            destWord.set(op1Word.add(op2Word));
-        });
-    }
-
-    toMachineCode() {
-        let code = this.startInstructionTypeC(this.args);
+        // setup instruction code
+        let code = '111';
         // get opCode and append
-        let prototype = Object.getPrototypeOf(this);
-        code += prototype.constructor.opCode;
+        let opCode = this.argsToCcode[this.args[0]];
+        code += opCode;
         // append params and destinations
-        code = this.noJump(this.createCodeFromArgs(this.args, code));
+        code += this.noJump(this.createDestCodeFrom(this.args));
 
         return code;
-
-    }
-}
-
-// A-D and M-D
-// TO-DO: D-A and D-M with opcode 010011
-export class SubwInstruction extends InsperHackInstruction {
-    static get mnemonic() {
-        return 'subw';
-    }
-    static get opCode() {
-        return '000111'; // A-D and M-D
     }
 
-    // generates assembly-text array args from code
-    static fromMachineCode(code) {
-        let params = this.setParams(code);
-        let dest = this.setDestinations(code);
-        let args = params.concat(dest);
-
-        return new SubwInstruction(args);
-    }
-
-    executeOn(system) {
-        // operand 1 reg/mem
+    executeOn(system) { // IN-PROGRESS
+        // operand 1 reg/mem/im
         let op1Word;
         if (this.isMemoryAccess(this.op1)) {
             op1Word = this.getMemoryValue(system);
-        } else {
+        } else if (this.op1.startsWith('%'))  { 
             op1Word = this.getRegValue(system, this.op1);
-        };
-        // operand 2 (op2) reg/mem/im
+        } else { 
+            op1Word = this.getImmediateValue(this.op1);
+        }
+        // operand 2 reg/mem
         let op2Word;
         if (this.isMemoryAccess(this.op2)) {
             op2Word = this.getMemoryValue(system);
-        } else if (this.op2.startsWith('%'))  { 
+        } else {
             op2Word = this.getRegValue(system, this.op2);
-        } else { 
-            op2Word = this.getImmediateValue(this.op2);
-        }
+        };
 
         this.dest.forEach((dest) => { 
             let destWord;
@@ -203,200 +189,10 @@ export class SubwInstruction extends InsperHackInstruction {
                 destWord = this.getRegValue(system, dest);
             }
             // set result word
-            destWord.set(op1Word.subtract(op2Word));
+            destWord.set(op1Word);
         });
     }
-
-    toMachineCode() {
-        // setup instruction code
-        let code = this.startInstructionTypeC(this.args);
-        // get opCode and append
-        let prototype = Object.getPrototypeOf(this);
-        code += prototype.constructor.opCode;
-        // append params and destinations
-        code = this.noJump(this.createCodeFromArgs(this.args, code));
-
-        return code;
-    }
 }
-
-export class RsubwInstruction extends InsperHackInstruction {
-
-}
-
-class OverwriteInstruction extends InsperHackInstruction {
-    toMachineCode() {
-        // setup instruction code
-        let code = this.startInstructionTypeC(this.args);
-        // get opCode and append
-        let prototype = Object.getPrototypeOf(this);
-        code += prototype.constructor.opCode;
-        // append params and destinations
-        code = this.noJump(this.createCodeFromArgs(this.args, code));
-
-        return code;
-    }
-}
-
-class IncwInstruction extends OverwriteInstruction {
-    static get mnemonic() {
-        return 'incw';
-    }
-    executeOn(system) {
-        // operand 1 reg
-        let op1Word;
-        if (this.isMemoryAccess(this.op1)) {
-            console.log("Error: Cannot read and write at the same time.");
-        } else {
-            op1Word = this.getRegValue(system, this.op1);
-            let destWord = op1Word; // same destination as given register
-            //destWord.set(op1Word.addImmediate(1));
-            destWord.set(op1Word.inc());
-            return destWord;
-        }
-    }    
-}
-
-export class IncAInstruction extends IncwInstruction {
-    static get opCode() {
-        return '110111'; // %A
-    }
-    static fromMachineCode(code) {
-        let arg = ['%A'];
-        return new IncAInstruction(arg);
-    }
-}
-
-export class IncDInstruction extends IncwInstruction {
-    static get opCode() {
-        return '011111'; // %D
-    }
-
-    static fromMachineCode(code) {
-        let arg = ['%D'];
-        return new IncDInstruction(arg);
-    }
-}
-
-class DecwInstruction extends OverwriteInstruction {
-    static get mnemonic() {
-        return 'decw';
-    }
-    executeOn(system) {
-        // operand 1 reg
-        let op1Word;
-        if (this.isMemoryAccess(this.op1)) {
-            console.log("Error: Cannot read and write at the same time.");
-            //throw new Error('Cannot read and write at the same time.');
-        } else {
-            op1Word = this.getRegValue(system, this.op1);
-            let destWord = op1Word; // same destination as given register
-            destWord.set(op1Word.dec());
-            return destWord;
-        }
-    }    
-}
-
-export class DecAInstruction extends DecwInstruction {
-    static get opCode() {
-        return '110010'; // %A
-    }
-    static fromMachineCode(code) {
-        let arg = ['%A'];
-        return new DecAInstruction(arg);
-    }
-}
-
-export class DecDInstruction extends DecwInstruction {
-    static get opCode() {
-        return '001110'; // %D
-    }
-
-    static fromMachineCode(code) {
-        let arg = ['%D'];
-        return new DecDInstruction(arg);
-    }
-}
-
-export class NotwInstruction extends InsperHackInstruction {
-    static get mnemonic() {
-        return 'notw';
-    }
-    executeOn(system) {
-        // operand 1 reg
-        let op1Word;
-        if (this.isMemoryAccess(this.op1)) {
-            console.log("Error: Cannot read and write at the same time.");
-            //throw new Error('Cannot read and write at the same time.');
-        } else {
-            op1Word = this.getRegValue(system, this.op1);
-            let destWord = op1Word; // same destination as given register
-            destWord.set(op1Word.dec());
-            return destWord;
-        }
-    } 
-}
-
-export class NotAInstruction extends NotwInstruction {
-    static get opCode() {
-        return '110001'; // %A
-    }
-    static fromMachineCode(code) {
-        let arg = ['%A'];
-        return new NotAInstruction(arg);
-    }
-}
-
-export class NotDInstruction extends NotwInstruction {
-    static get opCode() {
-        return '001101'; // %D
-    }
-
-    static fromMachineCode(code) {
-        let arg = ['%D'];
-        return new DecDInstruction(arg);
-    }
-}
-
-export class NegwInstruction extends InsperHackInstruction {
-
-}
-
-export class AndwInstruction extends InsperHackInstruction {
-
-}
-
-export class OrwInstruction extends InsperHackInstruction {
-
-}
-
-export class JmpInstruction extends InsperHackInstruction {
-
-}
-
-export class JeInstruction extends InsperHackInstruction {
-
-}
-export class JneInstruction extends InsperHackInstruction {
-
-}
-
-export class JlInstruction extends InsperHackInstruction {
-
-}
-
-export class JleInstruction extends InsperHackInstruction {
-
-}
-
-export class JgInstruction extends InsperHackInstruction {
-
-}
-
-export class JgeInstruction extends InsperHackInstruction {
-
-}
-
 export class NopInstruction extends InsperHackInstruction {
     static matchesCode(code) {
         let destCode = InsperHackInstruction.extractDestCode(code);
@@ -406,42 +202,8 @@ export class NopInstruction extends InsperHackInstruction {
     }
 }
 
-export class LabelInstruction extends InsperHackInstruction {
-
-}
-
-const instructionClasses = [
-    LeawInstruction,
-    MovwInstruction,
-
-
-    AddwInstruction,
-    SubwInstruction,
-    RsubwInstruction,
-
-    IncAInstruction,
-    IncDInstruction,
-    DecAInstruction,
-    DecDInstruction,
-
-    NotAInstruction,
-    NotDInstruction,
-
-    NegwInstruction,
-    AndwInstruction,
-    OrwInstruction,
-
-    JmpInstruction,
-    JeInstruction,
-    JneInstruction,
-
-    JlInstruction,
-    JleInstruction,
-
-    JgInstruction,
-    JgeInstruction,
-
-    NopInstruction,
-    LabelInstruction
-];
+const mnemonicToClass = {
+    'mov': MovInstruction,
+    'nop': NopInstruction,
+};
 
