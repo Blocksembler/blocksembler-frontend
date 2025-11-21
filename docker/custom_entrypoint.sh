572f0b9b2b5668d14d64a42ceaf530c541874ff9
Florian Wörister <31357619+fwoerister@users.noreply.github.com>
2025-11-21 12:41:00 +0100

    fix backend disabled mode (#191)

--- docker/custom_entrypoint.sh
+++ docker/custom_entrypoint.sh
@@ -3,12 +3,14 @@
 # Path to the runtime config.js file
 CONFIG_FILE=/usr/share/nginx/html/config.js
 
+export SERVER_NAME=${BLOCKSEMBLER_SERVER_NAME:-localhost}
+
 # Replace placeholders in config.js with environment variables
 cat <<EOF > $CONFIG_FILE
 window.env = {
     BACKEND_DISABLED: "${BLOCKSEMBLER_BACKEND_DISABLED:-false}",
     DEFAULT_ARCHITECTURE: "${BLOCKSEMBLER_DEFAULT_ARCHITECTURE:-anna}",
-    BACKEND_API_URL: "${BLOCKSEMBLER_BACKEND_API_URL:-http://localhost:8081}",
+    BACKEND_API_URL: "${BLOCKSEMBLER_BACKEND_API_URL:-http://localhost}",
     LOG_SYNC_BATCH_SIZE: "${BLOCKSEMBLER_LOG_SYNC_BATCH_SIZE}",
     LOG_SYNC_INTERVAL: "${BLOCKSEMBLER_LOG_SYNC_INTERVAL}",
     RESOURCE_LINKS: $(echo ${BLOCKSEMBLER_RESOURCE_LINKS} | jq -R -r -c @json)
